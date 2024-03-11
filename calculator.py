e69523d6bbd93028afd9bb0540ce4210b89a14bd
Josue Reyes <antoniojosuerp@ufps.edu.co>
2024-03-10 21:46:12 -0500

    Feat: the basic functional operations are added with two numbers

--- calculator.py
+++ calculator.py
@@ -1,1 +1,26 @@
-print("Hello word")
+  
+while True:
+    print("Hello world, this calculator is code 1152095")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))  
+
+    print("Options:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+    option = int(input("indicate the number of operation you want to perform:"))
+
+    if option == 5:
+        break
+    if option == 1:
+        print("Result:", num1, "+" , num2, "=" , num1+num2)
+    elif option == 2:
+        print("Result:", num1, "+" , num2, "=" , num1+num2)
+    elif option == 3:
+        print("Result:", num1, "+" , num2, "=" , num1+num2)
+    elif option == 4:
+        print("Result:", num1, "+" , num2, "=" , num1+num2)
+    else:
+        print("Invalid option")
